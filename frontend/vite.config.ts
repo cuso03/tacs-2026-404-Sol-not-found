891fb1295e17bfb0117e3ba0c029ea3d66c09aa3
Gonzalo Purita <gpurita@frba.utn.edu.ar>
2026-09-14 23:45:36 -0300

    Update: Front + Docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // mapea a 0.0.0.0 para que Docker exponga el puerto
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true, // Ayuda a que el hot-reload funcione bien en volúmenes de Windows a Docker
+    }
+  }
 })
